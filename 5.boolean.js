4db1b588299db82056039c3aa02fff57946478e7
yunna <homer_j@hanmail.net>
2023-04-17 19:57:27 +0900

    [4.6] boolean 타입

---
+++ 5.boolean.js
@@ -0,0 +1,27 @@
+// 불리언 타입
+let 참 = true;
+let 거짓 = false;
+console.log(참);
+console.log(거짓);
+
+// 활용 예 :
+let isFree = true;
+let isActivated = false;
+let isEnrolled = true;
+console.log(isActivated);
+
+console.clear();
+// Falsy 거짓인 값
+console.log(!!0);
+console.log(!!-0);
+console.log(!!'');
+console.log(!!null);
+console.log(!!undefined);
+console.log(!!NaN);
+
+// Truthy 참인 값
+console.log(!!1);
+console.log(!!-1);
+console.log(!!'text');
+console.log(!!{});
+console.log(!!Infinity);
