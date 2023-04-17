cabc6ec87d996c200020da2040be0696e0be06f2
yunna <homer_j@hanmail.net>
2023-04-17 20:34:51 +0900

    [4.7] null과 undefined

---
+++ 6.empty.js
@@ -0,0 +1,12 @@
+// null, undefined
+let variable;
+console.log(variable);
+
+variable = null;
+console.log(variable);
+
+let activeItem; // 아직 활성화된 아이템이 있는지 없는지 모르는 상태
+activeItem = null; // 활성화된 아이템이 없는 상태
+
+console.log(typeof null);
+console.log(typeof undefined);
