e1b1479503152e94db5b05db3a2c938c8d2d2515
yunna <homer_j@hanmail.net>
2023-04-17 20:52:55 +0900

    [4.8] 객체 타입

---
+++ 7.object.js
@@ -0,0 +1,11 @@
+// 연관된 데이터를 한번에 묶어서 보관할 수 있는 객체 (복합 데이터)
+let apple = {
+  name: 'apple',
+  color: 'red',
+  display: '🍎',
+};
+
+console.log(apple);
+console.log(apple.name);
+console.log(apple.color);
+console.log(apple.display);
