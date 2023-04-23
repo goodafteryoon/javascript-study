d4a97853d2666d7af2e33355476bbba009496b3d
yunna <homer_j@hanmail.net>
2023-04-24 08:38:21 +0900

    [5.8] 연산자 우선순위

---
+++ 7.priority.js
@@ -0,0 +1,8 @@
+let a = 2;
+let b = 3;
+let result = a + b * 4;
+console.log(result);
+result = a++ + b * 4;
+console.log(result);
+
+// MDN 연산자 우선순위: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
