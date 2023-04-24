0fb07d8c8f455e45ce95016385ebfa9415968d81
yunna <homer_j@hanmail.net>
2023-04-24 09:08:40 +0900

    [6.3] 삼항 연산자

---
+++ 2.ternary.js
@@ -0,0 +1,13 @@
+// 조건식 ? 참인 경우 : 거짓인 경우
+let fruit = 'apple';
+if (fruit === 'apple') {
+  console.log('🍎');
+} else {
+  console.log('👀');
+}
+
+fruit === 'apple' ? console.log('🍎') : console.log('👀');
+
+let emoji = fruit === 'apple' ? '🍎' : '👀';
+
+// MDN 삼항조건연산자: https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
