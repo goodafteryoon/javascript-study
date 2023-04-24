f0b9054301f971a981c4a69ee352389c8d67c11f
yunna <homer_j@hanmail.net>
2023-04-24 09:03:08 +0900

    [6.2] 조건문

---
+++ 1.if.js
@@ -0,0 +1,16 @@
+// 조건문 Conditional Statement
+// if(조건) { }
+// if(조건) { } else { }
+// if(조건1) { } else if(조건2) { } else { }
+let fruit = 'orange';
+if (fruit === 'apple') {
+  console.log('🍎');
+} else if (fruit === 'orange') {
+  console.log('🍊');
+} else {
+  console.log('👀');
+}
+
+if (2 < 1) {
+  console.log('출력되면 안됨');
+}
