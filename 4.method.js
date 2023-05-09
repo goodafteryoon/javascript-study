622adacfeda82e7c57f1745060ff1e1d2146b44c
yunna <homer_j@hanmail.net>
2023-05-09 23:14:24 +0900

    [8.5] 객체 안의 함수
    - 서로 연관있는 속성(데이터/프로퍼티)과 행동(함수/메소드)을 묶어 주기 위해
    
    - 객체 안에서 자기 자신의 속성을 접근할 때는 this를 붙여줘야한다.
    - 객체 안의 함수(메소드)는 화살표 함수가 아닌 일반 함수로 표기해야한다.

---
+++ 4.method.js
@@ -0,0 +1,12 @@
+// 서로 연관있는 속성과 행동을 묶어 둔 것이 객체
+const apple = {
+  name: 'apple',
+  display: function () {
+    console.log(`${this.name} : 🍎`); // 객체 안에서 자기 자신의 속성을 접근할 때는 this를 붙여줘야함
+  },
+  // display: () => {
+  //    console.log(`${this.name} : 🍎`);
+  // } // 화살표 함수의 this는 함수가 선언될 때 정적으로 결정되기 때문에 undefined로 출력된다.
+};
+
+apple.display();
