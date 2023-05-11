6cfdb23b51fd6abe868dc83333f0ec973725d7ff
yunna <homer_j@hanmail.net>
2023-05-11 10:50:16 +0900

    객체 폴더로 노트 파일 이동

--- 5.object/4.method.js
+++ 5.object/4.method.js
@@ -4,9 +4,9 @@ const apple = {
   display: function () {
     console.log(`${this.name} : 🍎`); // 객체 안에서 자기 자신의 속성을 접근할 때는 this를 붙여줘야함
   },
-  // display: () => {
-  //    console.log(`${this.name} : 🍎`);
-  // } // 화살표 함수의 this는 함수가 선언될 때 정적으로 결정되기 때문에 undefined로 출력된다.
+  display: () => {
+    console.log(`${this.name} : 🍎`);
+  }, // 화살표 함수의 this는 함수가 선언될 때 정적으로 결정되기 때문에 undefined로 출력된다.
 };
 
 apple.display();
