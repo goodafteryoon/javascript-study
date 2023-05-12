d262fdb20432016516c78cf4c49cee9ba4f7340b
yunna <homer_j@hanmail.net>
2023-05-12 09:14:14 +0900

    [9.4] 필드란
    - 프로퍼티의 접근을 제어하는 법
    - private(#)으로 지정하여 외부에선 접근이 불가능하게 할 수 있다.

---
+++ 3.field.js
@@ -0,0 +1,18 @@
+// 접근제어자 - 캡슐화
+// private(#), public(기본), protected
+class Fruit {
+  #name;
+  #emoji;
+  #type = '과일';
+  constructor(name, emoji) {
+    this.name = name;
+    this.emoji = emoji;
+  }
+  #display = () => {
+    console.log(`${this.name}: ${this.emoji}`);
+  };
+}
+
+const apple = new Fruit('apple', '🍎');
+// apple.#name = '오렌지'; // #field는 외부에서 접근이 불가능함
+console.log(apple);
