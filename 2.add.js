42ed7ac663ee30730ca507987229f130496faf1d
yunna <homer_j@hanmail.net>
2023-05-17 17:21:00 +0900

    [11.3] 하면 안되는 것
    - 배열의 인덱스를 이용해 배열을 추가하거나 삭제하는 것

---
+++ 2.add.js
@@ -0,0 +1,20 @@
+const fruits = ['🍌', '🍎', '🍇', '🍑'];
+
+// 배열 아이템을 참조하는 방법
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[3]);
+console.log(fruits.length);
+
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
+
+// const fruits = ['🍌', '🍎', '🍇', '🍑'];
+// 추가, 삭제 - 좋지 않은 방식 💩
+fruits[6] = '🍓';
+console.log(fruits);
+
+delete fruits[1];
+console.log(fruits);
