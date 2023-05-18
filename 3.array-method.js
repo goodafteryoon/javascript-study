23f0b16521e89893cc5f986628c34f4fe39eba10
yunna <homer_j@hanmail.net>
2023-05-18 19:12:57 +0900

    [11.5] 사용할 수 있는 함수 파트2
    - slice : 잘라서 새로운 배열로 반환
    - concat : 여러 배열을 합쳐서 새로운 배열로 반환
    - reverse : 배열 아이템의 순서를 거꾸로 해서 반환
    - flat : 중첩 배열을 param 숫자 만큼 펴주기
    - fill : param에 전달한 값으로 배열 채우기
    - join : param에 전달한 값으로 배열을 문자열로 합쳐주기

--- 3.array-method.js
+++ 3.array-method.js
@@ -35,3 +35,47 @@ console.log(fruits); // 배열 자체를 수정(업데이트)
 console.log(deleted);
 fruits.splice(1, 1, '🍎', '🍓');
 console.log(fruits); // 배열 자체를 수정(업데이트)
+
+// 잘라진 새로운 배열을 만듬
+let newArr = fruits.slice(0, 2); // 시작하는 인덱스는 포함, 끝나는 인덱스는 포함하지 않음
+console.log(newArr);
+console.log(fruits);
+newArr = fruits.slice(-1);
+console.log(newArr);
+
+// 여러개의 배열을 붙여줌
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const arr3 = arr1.concat(arr2);
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
+
+// 순서를 거꾸로
+console.log('1 arr3', arr3);
+const arr4 = arr3.reverse();
+console.log('2 arr3', arr3);
+console.log(arr4);
+
+// 중첩 배열을 하나의 배열로 쫙 펴기
+let arr = [
+  [1, 2, 3],
+  [4, [5, 6]],
+];
+console.log(arr);
+console.log(arr.flat());
+console.log(arr.flat(2));
+arr = arr.flat(2);
+
+// 특정한 값으로 배열을 채우기
+arr.fill(0); // 배열 자체를 수정
+console.log(arr);
+
+arr.fill('s', 1, 3);
+console.log(arr);
+
+// 배열을 문자열로 합하기
+let text = arr.join();
+console.log(text);
+text = arr.join(' | ');
+console.log(text);
