0c8adff2273d8ce71150f9ca77bb6ec7b9f885ad
yunna <homer_j@hanmail.net>
2023-07-16 16:11:28 +0900

    [15.4] 모듈 작성법

---
+++ counter.js
@@ -0,0 +1,8 @@
+let count = 0;
+export function increase() {
+  count++;
+  console.log(count);
+}
+export function getCount() {
+  return count;
+}
---
+++ main.js
@@ -0,0 +1,8 @@
+// import { increase as increase1 } from './counter.js';
+// import { increase, getCount } from './counter.js';
+import * as counter from './12.module/counter.js';
+
+counter.increase();
+counter.increase();
+counter.increase();
+console.log(counter.getCount());
