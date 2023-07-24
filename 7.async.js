3de87bb2fff5c61d28a4cc1b3a4e359f39b6c330
yunna <homer_j@hanmail.net>
2023-07-24 10:28:57 +0900

    [16.9] 비동기 코드를 동기적으로 async, await

---
+++ 7.async.js
@@ -0,0 +1,29 @@
+function getBanana() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('🍌');
+    }, 1000);
+  });
+}
+
+function getApple() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('🍎');
+    }, 3000);
+  });
+}
+
+function getOrange() {
+  return Promise.reject(new Error('no orange'));
+}
+
+// 바나나와 사과를 같이 가지고 오기
+async function fetchFruits() {
+  const banana = await getBanana();
+  const apple = await getApple();
+  return [banana, apple];
+}
+
+fetchFruits() //
+  .then((fruits) => console.log(fruits));
