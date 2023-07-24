3bb82a466412639e5cb18b65c81c1f855d66a7d2
yunna <homer_j@hanmail.net>
2023-07-24 10:38:53 +0900

    [16.10 ~ 16.11] async, await 퀴즈

---
+++ 8.async-quiz.js
@@ -0,0 +1,23 @@
+function fetchEgg(chicken) {
+  return Promise.resolve(`${chicken} => 🥚`);
+}
+function fryEgg(egg) {
+  return Promise.resolve(`${egg} => 🍳`);
+}
+function getChicken() {
+  return Promise.reject(new Error('can not find 🐓'));
+}
+
+async function makeFriedEgg() {
+  let chicken;
+  try {
+    chicken = await getChicken();
+  } catch {
+    chicken = '🐔';
+  }
+  const egg = await fetchEgg(chicken);
+  return await fryEgg(egg);
+}
+
+makeFriedEgg() //
+  .then(console.log);
