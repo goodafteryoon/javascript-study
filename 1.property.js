00ea506f76a036885066a2e80f489157a5a5ae52
yunna <homer_j@hanmail.net>
2023-08-05 20:51:47 +0900

    [18.3] 프로퍼티 디스크립터

---
+++ 1.property.js
@@ -0,0 +1,54 @@
+const dog = { name: '와우', emoji: '🐶' };
+
+console.log(Object.keys(dog));
+console.log(Object.values(dog));
+console.log(Object.entries(dog));
+
+console.log('name' in dog);
+console.log(dog.hasOwnProperty('name'));
+
+// 오브젝트의 각각의 프로퍼티는 프로퍼티 디스크립터라고 하는 객체로 저장됨
+const descriptors = Object.getOwnPropertyDescriptors(dog);
+console.log(descriptors);
+
+const desc = Object.getOwnPropertyDescriptor(dog, 'name');
+console.log(desc);
+
+Object.defineProperty(dog, 'name', {
+  value: '멍멍',
+  writable: false,
+  enumerable: false,
+  configurable: false,
+});
+
+console.log(dog.name);
+console.log(Object.keys(dog));
+delete dog.name;
+console.log(dog.name);
+
+const student = {};
+Object.defineProperties(student, {
+  firstName: {
+    value: '영희',
+    writable: true,
+    enumerable: true,
+    configurable: true,
+  },
+  lastName: {
+    value: '김',
+    writable: true,
+    enumerable: true,
+    configurable: true,
+  },
+  fullName: {
+    get() {
+      return `${this.lastName} ${this.firstName}`;
+    },
+    set(name) {
+      [this.lastName, this.firstName] = name.split(' ');
+    },
+    configurable: true,
+  },
+});
+console.log(student);
+console.log(student.fullName);
