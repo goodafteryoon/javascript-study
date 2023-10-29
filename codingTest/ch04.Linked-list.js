1c1efc291b362ca968367975a5efc4f919ab148e
yunna <homer_j@hanmail.net>
2023-10-29 18:26:22 +0900

    [ch04] 연결 리스트

---
+++ codingTest/ch04.Linked-list.js
@@ -0,0 +1,75 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  // 찾기 로직
+  find(value) {
+    let currNode = this.head;
+    while (currNode.value !== value) {
+      currNode = currNode.next;
+    }
+    return currNode;
+  }
+
+  // 끝 부분 추가
+  append(newValue) {
+    const newNode = new Node(newValue);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  insert(node, newValue) {
+    const newNode = new Node(newValue);
+    newNode.next = node.next;
+    node.next = newNode;
+  }
+
+  remove(value) {
+    let prevNode = this.head;
+    while (prevNode.next.value !== value) {
+      prevNode = prevNode.next;
+    }
+
+    if (prevNode.next !== null) {
+      prevNode.next = prevNode.next.next;
+    }
+  }
+
+  display() {
+    let currNode = this.head;
+    let displayString = '[';
+    while (currNode !== null) {
+      displayString += `${currNode.value}, `;
+      currNode = currNode.next;
+    }
+    displayString = displayString.substr(0, displayString.length - 2);
+    displayString += ']';
+    console.log(displayString);
+  }
+}
+
+const linkedList = new SinglyLinkedList();
+
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+linkedList.append(5);
+linkedList.display();
+console.log(linkedList.find(2));
+linkedList.remove(3);
+linkedList.insert(linkedList.find(2), 10);
+linkedList.display();
